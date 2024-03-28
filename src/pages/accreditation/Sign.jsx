9b7d8e06f7850afdc797af6766aa717448ae1777
infodavid00 <da40au40@gmail.com>
2024-03-28 07:24:37 +0100

    d

--- src/pages/accreditation/Sign.jsx
+++ src/pages/accreditation/Sign.jsx
@@ -1,63 +1,42 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import SignaturePad from './SignatureCanvas';
 import './signaturecanvas.css';
-import './accreditation.css';
+// import './accreditation.css';
 
 function SignaturePanel({ onSignatureChange, onDateChange }) {
   const [signature, setSignature] = useState(null);
   const [date, setDate] = useState('');
-  const [isMounted, setIsMounted] = useState(false);
-  const titleRef = useRef();
 
   const handleSave = (dataUrl) => {
     setSignature(dataUrl);
+    // Pass the signature data to the parent component
     onSignatureChange(dataUrl);
   };
 
   const handleDateChange = (event) => {
     const { value } = event.target;
     setDate(value);
+    // Pass the date data to the parent component
     onDateChange(value);
-  };
-
-  const handleMount = () => {
-    setIsMounted(true);
   };
 
   return (
     <>
-      <div onLoad={handleMount}>
-        <h3 id='accreditation-signimage-sig-cont-text'>Signature:</h3>
-        <br />
-        <br />
-        <SignaturePad onSave={handleSave} />
-        <br />
-        <br />
-        <div className='accreditation-box-subbox'>
-          <div className='accreditation-box-input-title'>Date</div>
-          <input
-            type="date"
-            required={true}
-            className='accreditation-box-input-text'
-            value={date}
-            onChange={handleDateChange}
-          />
-        </div>
+    {/* //   <h3 id='accreditation-signimage-sig-cont-text'>Signature:</h3>
+    //   <br/> <br /> */}
+      <SignaturePad onSave={handleSave} />
+      {/* // <br /> 
+      // <br /> */}
+      <div className='accreditation-box-subbox'>
+        <div className='accreditation-box-input-title'>Date</div>
+        <input 
+          type="date"  
+          required={true}
+          className='accreditation-box-input-text'
+          value={date}
+          onChange={handleDateChange}
+        />
       </div>
-      {!isMounted && (
-        <div>
-          <p>
-            Initialize your scenario here. The content will only be displayed before
-            the component mounts.
-          </p>
-          <input
-            ref={titleRef}
-            type="text"
-            defaultValue={"Enter a title here"}
-          />
-          <button onClick={() => setIsMounted(true)}>Mount</button>
-        </div>
-      )}
     </>
   );
 }
